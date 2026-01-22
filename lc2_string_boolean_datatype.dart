74d211f36b371767013e8b913a26cea8cd363546
sushantm1 <sushantm1100202@gmail.com>
2026-01-22 10:11:31 +0530

    final commit of this lecture

--- lc2_string_boolean_datatype.dart
+++ lc2_string_boolean_datatype.dart
@@ -20,7 +20,25 @@ void main() {
 
   // q2
 
-  var n = stdin.readLineSync()!;
+  // var n = stdin.readLineSync()!;
 // var n=10;
-  print(n.runtimeType);
+  // print(n.runtimeType);
+  //input decimal number
+
+  // q3 take input from the user and store it in double datatype
+  // print the exact datatype and also print the value upto 3 decimal places
+  // double decNum1 = stdin.readLineSync()!;
+  // var decNum = stdin.readLineSync()!;
+  // print(decNum.runtimeType);
+  // double dNum = double.parse(decNum);
+  // print(dNum.runtimeType);
+  // print(dNum.toStringAsFixed(3));
+
+  // q4
+  var boolInput = stdin.readLineSync()!;
+  print(boolInput.runtimeType);
+  bool b1 = bool.parse(boolInput);
+  print(b1.runtimeType);
+  print(b1);
+  print(!b1);
 }
