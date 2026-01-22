98ffd6d5bbfa40903181c93e1ec6e33a065b3a51
sushantm1 <sushantm1100202@gmail.com>
2026-01-22 09:44:33 +0530

    lec 2

--- lc2_string_boolean_datatype.dart
+++ lc2_string_boolean_datatype.dart
@@ -4,5 +4,23 @@ void main() {
   // String name = "BabuRao";
   // print(name);
 
-  String name = stdin.readLineSync()!;
+  // String name = stdin.readLineSync()!;
+  // print(name);d
+
+  // int char = stdin.readByteSync();
+  // print(char);
+  // print(String.fromCharCode(char));
+
+  // q1
+  // String char1 = stdin.readLineSync()!;
+  // String char2 = stdin.readLineSync()!;
+  // int a1 = int.parse(char1);
+  // int a2 = int.parse(char2);
+  // print(a1 + a2);
+
+  // q2
+
+  var n = stdin.readLineSync()!;
+// var n=10;
+  print(n.runtimeType);
 }
